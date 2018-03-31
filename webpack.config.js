cb72a6a3d0e46cc1868d4f3e39d877e753dfa679
Raphael Okure <raphael@arccosgolf.com>
2018-03-31 12:24:32 -0700

    * 0.1.3: changd name of (and path to) default export

--- webpack.config.js
+++ webpack.config.js
@@ -10,7 +10,7 @@ const path = require('path');
 module.exports = {
     entry: path.resolve(__dirname, 'src/index.js'),
     output: {
-        path: path.resolve(__dirname, 'build'),
+        path: path.resolve(__dirname, 'lib'),
         filename: 'raphsducks.js'
     },
     module: {
