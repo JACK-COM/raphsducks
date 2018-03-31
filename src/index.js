a805a07e46c72134b07348794d3ee9416daee4e4
Raphael Okure <raphael@arccosgolf.com>
2018-03-31 12:34:52 -0700

    * 0.1.4: ...fixed default export and file name

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ let Reducers = {};
 const assertIsFunction = object => typeof object === "function";
 const makeNullAction = type => ({ type, payload: null });
 
-module.exports = function create(reducers, initialState = null) {
+module.exports.default = function create(reducers, initialState = null) {
     const keys = [];
     // Initialize reducers
     Object.keys(reducers).forEach(key => {
