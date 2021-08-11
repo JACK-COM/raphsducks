702f0e5444685d981585c248e396a0de2bdeb87e
MrJackdaw <MrJackdaw@users.noreply.github.com>
2021-08-10 19:58:04 -0700

    * updates babelrc * updates webpack config * removes unused bundle

--- webpack.config.js
+++ webpack.config.js
@@ -25,12 +25,12 @@ module.exports = {
   module: {
     rules: [
       {
-        test: /\.js$/,
+        test: /\.jsx?$/,
         exclude: /(node_modules)/,
         use: {
           loader: "babel-loader",
           options: {
-            presets: ["babel-preset-env"],
+            // presets: ["babel-preset-env"],
           },
         },
       },
